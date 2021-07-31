87dc7a27043cc77311a488cf0d54d53f1b4f6903
Hawk56767 <76022450+Hawk56767@users.noreply.github.com>
2021-07-31 12:18:34 -0400

    Add files via upload

---
+++ C82CanvasPaint/main.js
@@ -0,0 +1,41 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+var mouse_event="empty";
+var lastpositionofx,lastpositionofy;
+color="black";
+widthofline=9;
+canvas.addEventListener("mousedown",mymousedown)
+function mymousedown(e){
+mouse_event="mouseDown";
+color=document.getElementById("color").value;
+widthofline=document.getElementById("line").value
+}
+canvas.addEventListener("mouseup",mymouseup)
+function mymouseup(e){
+mouse_event="mouseUp";
+
+}
+canvas.addEventListener("mouseleave",mymouseleave)
+function mymouseleave(e){
+
+mouse_event="mouseleave";
+
+}
+canvas.addEventListener("mousemove",mymousemove)
+function mymousemove(e){
+console.log("test");
+current_positon_of_mouseX=e.clientX-canvas.offsetLeft;
+current_positon_of_mouseY=e.clientY-canvas.offsetTop;
+if(mouse_event=="mouseDown"){
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=widthofline;
+ctx.moveTo(lastpositionofx,lastpositionofy);
+ctx.lineTo(current_positon_of_mouseX,current_positon_of_mouseY);
+ctx.stroke();
+}
+lastpositionofx=current_positon_of_mouseX;
+lastpositionofy=current_positon_of_mouseY;
+
+}
+
